d2b9f437f71d2c526403ac0e6dcdf1614798437d
WSmithDR <wagnersmith123@hotmail.com>
2023-08-06 23:04:02 -0500

    resueltos los ejercicios 6 y 7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
+import axios from 'axios';
 import './App.css';
 import Cards from './components/Cards.jsx';
 import NavBar from './components/NavBar';
 import {useState} from "react"
 
-const example = {
-   id: 1,
-   name: 'Rick Sanchez',
-   status: 'Alive',
-   species: 'Human',
-   gender: 'Male',
-   origin: {
-      name: 'Earth (C-137)',
-      url: 'https://rickandmortyapi.com/api/location/1',
-   },
-   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-}
 
 function App() {
    const [characters, setCharacters] = useState([])
-   const onSearch = ()=>{
-      setCharacters([...characters, example])
+   const onSearch = (id)=>{
+      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(({data})=>{
+         if(data.id){
+            setCharacters([...characters, data])
+         }else window.alert("¡No hay personaje con este Id!")
+      })
    }
    
    return (
