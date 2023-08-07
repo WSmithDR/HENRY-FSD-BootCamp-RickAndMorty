a1a78fc79052ea07c53e1a657bc9f5d1448e3305
WSmithDR <wagnersmith123@hotmail.com>
2023-08-06 22:09:19 -0500

    Resuelto los dos primeros ejercicios.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
-export default function Card(props) {
+export default function Card({
+   id,
+   name,
+   status,
+   species,
+   gender,
+   origin,
+   image,
+   onClose
+}) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={onClose}>X</button>
+         <p><strong>Name: </strong>{name}</p>
+         <p><strong>Status: </strong>{status}</p>
+         <p><strong>Species: </strong>{species}</p>
+         <p><strong>Gender: </strong>{gender}</p>
+         <p><strong>Origin: </strong>{origin}</p>
+         <img src={image} alt={name} />
       </div>
    );
 }
