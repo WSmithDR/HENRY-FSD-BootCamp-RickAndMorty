8ea57317e2da878b45c9bfd4b72a12d321473616
WSmithDR <wagnersmith123@hotmail.com>
2023-08-08 22:28:04 -0500

    resuelto el ejercicio 3

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react"
+
 const Form = () => {
+    const [userData, setUserData] = useState({email:"", password:""})
+    const handleChange = (event)=>{
+        setUserData({...userData, [event.target.name]:event.target.value})
+    }
     return(
         <form>
             <div>
                 <label htmlFor="email">Email: </label>
-                <input type="email"/>
+                <input type="email" onChange={handleChange} value={userData.email} name="email"/>
             </div>
             <div>
                 <label htmlFor="password">Password: </label>
-                <input type="password"/>
+                <input type="password" onChange={handleChange} value={userData.password} name="password"/>
             </div>
             <div>
                 <button>Submit</button>
