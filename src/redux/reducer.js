7e6f5828d50fe3f0641f2237b06d096c10388ab2
WSmithDR <wagnersmith123@hotmail.com>
2023-08-24 15:57:50 -0500

    nuevos cambios porque fregué la anterior.

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -6,21 +6,14 @@ const initialState = {
 
 const reducer = (state = initialState, action)=> {
     switch(action.type){
-        
         case ADD_FAV:
-            return {...state, 
-                myFavorites:[...state.myFavorites, 
-                    action.payload]}
+            return {...state,
+            myFavorites:[...state.myFavorites, action.payload]}
         
         case REMOVE_FAV:
-            const filteredCharacters = state.myFavorites
-            .filter(character => character
-                .id !== Number(action.payload))
-            return {
-                ...state,
-                myFavorites: filteredCharacters
-            }
-        
+            return {...state,
+            myFavorites: state.myFavorites
+            .filter(character=> character.id !== action.payload)}
         default:
             return {...state}
     }
