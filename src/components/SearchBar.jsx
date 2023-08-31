3138025c32fa724b6f650bbfdeecaa2e8bb449ee
WSmith <wagnersmith123@hotmail.com>
2023-08-31 14:28:40 -0500

    creando plantilla

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function SearchBar({onSearch}) {
+   const [id, setId] = useState("")
+   const handleInputChange = (event) => {
+      setId(event.target.value)
+   }
    return (
       <div>
-         <input type='search' />
+         <input type='search' onChange={handleInputChange} value={id}/>
          <button onClick={onSearch}>Agregar</button>
       </div>
    );
