eb8338f9fcd545acb9aee7c1dde5d47bd352d1db
WSmith <wagnersmith123@hotmail.com>
2023-09-14 22:01:43 -0500

    plantilla

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,24 @@
 const express = require("express")
 const server = express()
 const PORT = 3001
+const router = require("./routes/index")
+
+server.use(express.json())
+
+server.use((request, response, next)=> {
+    response.header("Access-Control-Allow-Origin","*")
+    response.header("Access-Control-Allow-Credentials", "true")
+    response.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-with, Content-Type, Accept"
+    )
+    response.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, DELETE"
+    )
+    next()
+})
+
+server.use("/rickandmorty", router)
 
 server.listen(PORT, ()=> console.log(`Server raised in port ${PORT}`))
