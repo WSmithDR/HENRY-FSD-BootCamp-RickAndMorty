3be0615a072391ddb372c7e2d181b3ffddeee34b
WSmith <wagnersmith123@hotmail.com>
2023-09-14 20:53:17 -0500

    plantilla

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,10 +1,26 @@
+const URL = `https://rickandmortyapi.com/api/character`
 const axios = require("axios")
-const getCharById = (response, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`)
+
+const getCharById = (request, response) => {
+    const {id} = request.params
+    axios(`${URL}/${id}`)
     .then(response => response.data)
-    .then(character => response
-        .writeHead(200,{"Content-Type":"Application/json"})
-        .end(JSON.stringify(character)))
+    .then(({id, name, status, species, gender, origin, image }) => {
+        if(name){
+            const character = {
+                id,
+                name,
+                species,
+                origin,
+                image,
+                gender,
+                status
+            }
+            return response.status(200).json(character)
+        }
+        return response.status(404).send(`Not found`)
+    })
+    .catch(error => response.status(500).send(error.message))
 }
 
 module.exports = {
