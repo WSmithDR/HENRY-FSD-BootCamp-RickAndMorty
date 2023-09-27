bc1510a8071512936bc68a880845e0abe119efe3
WSmith <wagnersmith123@hotmail.com>
2023-09-27 12:24:24 -0500

    plantilla

---
+++ Server/test/index.test.js
@@ -0,0 +1,12 @@
+const app = require("./../src/app")
+const session = require("supertest")
+const request = session(app)
+
+describe("test de RUTAS", ()=>{
+    describe("GET /rickandmorty/character/:id", ()=>{
+        it("Responde con status: 200", async ()=>{
+            const response = await request.get("/rickandmorty/character/1")
+            expect(response.statusCode).toBe(200)
+        })
+    })
+})
