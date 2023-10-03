777f5129a9c28ade0b754ac7993cd82a1b100ed0
WSmith <wagnersmith123@hotmail.com>
2023-10-03 16:53:21 -0500

    plantilla

---
+++ Server/src/DB_connection.js
@@ -0,0 +1,7 @@
+require("dotenv").config()
+const {Sequelize} = require("sequelize")
+const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`,
+{logging: false, native:false})
