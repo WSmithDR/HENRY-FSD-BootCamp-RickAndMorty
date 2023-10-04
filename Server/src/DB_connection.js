cfdb05319d385b9f22e34e3e47d927bb99216b41
WSmith <wagnersmith123@hotmail.com>
2023-10-04 10:37:57 -0500

    plantilla

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,7 +1,14 @@
 require("dotenv").config()
 const {Sequelize} = require("sequelize")
 const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+const {FavoriteModel} = require("./../src/models/Favorite")
+const {UserModel} = require("./../src/models/User")
 
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`,
 {logging: false, native:false})
+
+FavoriteModel(sequelize)
+
+UserModel(sequelize)
+
