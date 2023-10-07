cd8ed580deb2bf57b2bd789d9163db5262e76595
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-07 13:41:38 -0500

    plantilla

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,19 +1,25 @@
-const User = require("./../DB_connection")
+const {User} = require("./../DB_connection")
 
 const login = async (request, response) => {
     try {
         const {email, password} = request.query
-        if(!email || !password) return response.status(400).send("No data enough!")
+        if(!email || !password) return response.status(400).send("Not enough data!")
+    
         const user = await User.findOne({
             where:{
                 email
             }
         })
-        if(password !== user.password) return response.status(403).send("Incorrect password")
-        return response.status(200).json({access: true})
+
+        if(!user) return response.status(404).send("User not found!")
+
+        return user.password === password
+        ? response.status(200).json({access: true})
+        : response.status(403).send("Incorrect password")
     } catch (error) {
         return response.status(500).json({error: error.message})
-    }   
+    }
+
 }
 
 module.exports = login
