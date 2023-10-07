1c2850b6e13b1e5ca6d68f94cae695b206f3c261
WagnerSmith <wagnersmith123@hotmail.com>
2023-10-07 12:09:01 -0500

    plantilla

---
+++ Server/src/controllers/postUser.js
@@ -0,0 +1,22 @@
+const User = require("./../DB_connection")
+
+const postUser = async (request, response) => {
+    try {
+        const {email, password} = request.body
+        if(!email || !password) return response.status(400).send("No enough data!")
+        const user = await User.findOrCreate({
+            where: {
+                email,
+                password
+            }
+        })
+        
+        return response.status(200).json(user)
+    } catch (error) {
+        return response.status(500).json({error: error.message})
+    }
+}
+
+module.exports = {
+    postUser
+}
