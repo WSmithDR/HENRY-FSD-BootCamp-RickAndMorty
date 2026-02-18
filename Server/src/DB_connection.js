3faecb29499739a8311ec1070b3c8cf96f2975ed
SmithDR <wsduenas@espol.edu.ec>
2026-02-18 13:19:07 -0500

    updated template

--- Server/src/DB_connection.js
+++ Server/src/DB_connection.js
@@ -1,7 +1,19 @@
 require("dotenv").config()
 const {Sequelize} = require("sequelize")
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}`,
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 {logging: false, native:false})
+
+
+const test_db_connection = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log("DB connection stablished successfully!")
+    } catch (error) {
+        console.error("Error DB connection: ", error)
+    }
+}
+
+test_db_connection()
