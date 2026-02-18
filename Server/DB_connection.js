3b5d19fe60f715b256447831493986fce16a1e0a
SmithDR <wsduenas@espol.edu.ec>
2026-02-18 12:54:18 -0500

    created template

--- Server/DB_connection.js
+++ Server/DB_connection.js
@@ -1,8 +1,6 @@
 require(`dotenv`).config()
 const {Sequelize} = require(`sequelize`)
 const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
-const FavoriteModel = require("./Models/Favorite")
-const UserModel = require("./Models/User")
 
 
 
@@ -22,15 +20,3 @@ const test_db_connection = async () =>{
 test_db_connection()
 
 
-FavoriteModel(sequelize)
-UserModel(sequelize)
-
-const {User, Favorite} = sequelize.models
-User.belongsToMany(Favorite, {through: 'user_favorite', timestamps: false})
-Favorite.belongsToMany(User, {through: 'user_favorite', timestamps: false})
-
-module.exports = {
-    User,
-    Favorite,
-    conn: sequelize
-}
