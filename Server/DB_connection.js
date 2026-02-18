a48c056dd376372c1a1ad27bf366a77dd5fde701
SmithDR <wsduenas@espol.edu.ec>
2026-02-18 12:51:20 -0500

    updated template

--- Server/DB_connection.js
+++ Server/DB_connection.js
@@ -1,14 +1,25 @@
 require(`dotenv`).config()
 const {Sequelize} = require(`sequelize`)
-const {DB_USER, DB_PASSWORD, DB_HOST} = process.env
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
 const FavoriteModel = require("./Models/Favorite")
 const UserModel = require("./Models/User")
 
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
 {logging: false, native: false}
 )
+
+const test_db_connection = async () =>{
+    try {
+        await sequelize.authenticate()
+        console.log("Db connection stablished")
+    } catch (error) {
+        console.error("Error", error)
+    }
+}
+
+test_db_connection()
 
 
 FavoriteModel(sequelize)
