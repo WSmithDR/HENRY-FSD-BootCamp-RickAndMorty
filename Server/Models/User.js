d777994c60b17a6a583e74dd7c2b80eb719d069d
SmithDR <wsduenas@espol.edu.ec>
2026-02-22 15:54:02 -0500

    feat: UserModel enhanced with password restctions

--- Server/Models/User.js
+++ Server/Models/User.js
@@ -1,4 +1,5 @@
 const {DataTypes} = require("sequelize")
+const bcrypt = require('bcryptjs')
 
 module.exports = sequelize => {
     sequelize.define("User", {
@@ -10,12 +11,34 @@ module.exports = sequelize => {
         }, 
         email: {
             type: DataTypes.STRING,
-            allowNull: true,
-            isEmail: true
+            allowNull: false,
+            isEmail: true,
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            set(value) {
+                const salt = bcrypt.genSaltSync(10)
+                const hash = bcrypt.hashSync(value, salt)
+                this.setDataValue('password', hash)
+            }
         }
-    }, {timestamps: false})
+    }, {
+        timestamps: false,
+        hooks: {
+            beforeCreate: (user) => {
+                if (user.password) {
+                    const salt = bcrypt.genSaltSync(10)
+                    user.password = bcrypt.hashSync(user.password, salt)
+                }
+            },
+            beforeUpdate: (user) => {
+                if (user.changed('password')) {
+                    const salt = bcrypt.genSaltSync(10)
+                    user.password = bcrypt.hashSync(user.password, salt)
+                }
+            }
+        }
+    })
 }
