8575a7d921eed2216319fe8360ecd7a7645f42c6
SmithDR <wsduenas@espol.edu.ec>
2026-04-02 23:08:26 -0500

    created template

--- Server/src/middlewares/auth.js
+++ Server/src/middlewares/auth.js
@@ -2,36 +2,6 @@ const jwt = require('jsonwebtoken')
 const { jwtSecret } = require('../config/env')
 
 
-const authMiddleware = (req, res, next) => {
-    try {
-        // Obtener token del header
-        const authHeader = req.headers.authorization
-        if (!authHeader) {
-            return res.status(401).json({ error: 'Token not provided' })
-        }
-
-        // Extraer token (quitar "Bearer ")
-        const token = authHeader.split(' ')[1]
-        if (!token) {
-            return res.status(401).json({ error: 'Token format invalid' })
-        }
-
-        // Verificar token
-        const decoded = jwt.verify(token, jwtSecret)
-
-        // Añadir usuario al request
-        req.user = decoded
-        next()
-
-    } catch (error) {
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({ error: 'Invalid token' })
-        }
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ error: 'Token expired' })
-        }
-        return res.status(500).json({ error: 'Authentication error' })
-    }
-}
+const authMiddleware = 
 
 module.exports = authMiddleware
--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -3,20 +3,17 @@ const getCharById = require("../controllers/getCharById")
 const postFav = require("./../controllers/postFav")
 const deleteFav = require("./../controllers/deleteFav")
 const postUser = require("./../controllers/postUser")
-const authMiddleware = require("../middlewares/auth")
 
 const router = require("express").Router()
 
-router.get("/character/:id", (request, response) =>{
-    getCharById(request, response)
-})
+router.get("/character/:id", getCharById)
 
 router.post("/login", login)
 
 // RUTAS PROTEGIDAS con middleware
-router.post("/fav", authMiddleware, postFav)
+router.post("/fav", postFav)
 
-router.delete("/fav/:id", authMiddleware, deleteFav)
+router.delete("/fav/:id", deleteFav)
 
 router.post("/register", postUser)
 
