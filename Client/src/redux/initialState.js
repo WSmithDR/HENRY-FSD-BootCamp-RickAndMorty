4745d05a9175d46d6845f10da5ad008f5631e916
SmithDR <wsduenas@espol.edu.ec>
2026-04-12 00:07:25 -0500

    feat: updated template

---
+++ Client/src/redux/initialState.js
@@ -0,0 +1,13 @@
+
+
+const initialState = {
+    myFavorites: [],
+    allCharacters: [],
+    user: null,
+    token: null,
+    access: false,
+    isLoading: false,
+    error: null
+}
+
+export default initialState
